7ad86a5ce92ac01f424b55cd5e410c4e20b5d617
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2012-02-19 21:39:46 -0500

    youtube checking

--- public/js/modules/ytube.js
+++ public/js/modules/ytube.js
@@ -7,21 +7,26 @@ define(function() {
 	{
 		// embed youtube
 		var params		= {allowScriptAccess: "always", "allowFullScreen": false};
-		var atts 		= {id: "myytplayer"};
+		var atts 		= {id: "ytplayer"};
 		var flashvars	= {};
 
-		swfobject.embedSWF("http://www.youtube.com/apiplayer?version=3&enablejsapi=1&playerapiid=ytplayer", 
+		swfobject.embedSWF("http://www.youtube.com/v/8UVNT4wvIGY?version=3&enablejsapi=1&playerapiid=ytplayer", 
 			"ytapiplayer", "470", "200", "9", null, flashvars, params, atts);
 	}
 
 	function onYouTubePlayerReady(playerId) {
-		console.log("check event");
-		ytplayer = document.getElementById('myytplayer');
+		console.log("fjlj")
+		ytplayer = document.getElementById(playerId);
 		ytplayer.addEventListener("onError", "onPlayerError");
+		ytplayer.addEventListener("onStateChange", "onytplayerStateChange");
 	}
 
 	function onPlayerError(errorCode) {
 		alert("An error occured of type:" + errorCode);
+	}
+
+	function onytplayerStateChange(newState) {
+		alert("Player's new state: " + newState);
 	}
 
 	return {
