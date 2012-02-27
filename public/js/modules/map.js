039cf68226588702a799bba01ad52d57f3d38ed8
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2012-02-27 12:20:24 -0500

    for dev

--- public/js/modules/map.js
+++ public/js/modules/map.js
@@ -5,8 +5,24 @@ define(['jquery'], function($) {
 			var brooklyn = function() {
 				return $('#DV-bk');
 			}
+			var manhattan = function() {
+				return $('#DV-man');
+			}
+			var queens = function() {
+				return $('#DV-queens');
+			}
+			var staten = function() {
+				return $('#DV-staten');
+			}
+			var bronx = function() {
+				return $('#DV-bronx');
+			}
 			return {
-				brooklyn: brooklyn()
+				brooklyn: brooklyn(),
+				manhattan: manhattan(),
+				queens: queens(),
+				staten: staten(), 
+				bronx: bronx()
 			};
 		})();
 
@@ -17,8 +33,8 @@ define(['jquery'], function($) {
 		log(DVmap)
 		$('.borough').on('click', function(event) {
 			$('html, body').animate({
-				scrollTop: DVmap[event.target.id].offset().top
-			}, 2000);
+				scrollTop: DVmap[event.target.id].offset().top - $('.navbar').height()
+			}, 300);
 			return false;
 		});
 	}
