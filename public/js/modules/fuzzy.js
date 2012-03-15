b507056f760c182910eaa5c90456f741ea1bc655
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2012-03-15 17:24:33 -0400

    disabled abled

--- public/js/modules/fuzzy.js
+++ public/js/modules/fuzzy.js
@@ -1,19 +1,19 @@
 define(['jquery'], function($) {
+	var urlpath = window.location.protocol + "//" + window.location.host;
 
 	function _init() {
-		var urlpath = window.location.protocol + "//" + window.location.host;
-		loadContent(urlpath);
-		onSubmitModal(urlpath);
+		loadContent();
 	}
 
 	function onSubmitModal(site) {
-		$('#submitED').on('click', function() {
+		$('#submitED').on('click', function(e) {
+			e.preventDefault();
 			var number = $('#frm-modal-number').val() || null;
-			var streetName = $('#frm-modal-street').val() || null;
 			var name = $('#frm-modal-name').val() || null;
 			var borough = environment.borough;
 			var state = $('#state_hidden').val();
 			var fullcity = $('#fullcity_hidden').val();
+			var streetName = $('#frm-modal-street').val() || null;
 
 			if (streetName !== null && state !== null && fullcity !== null && $.inArray(streetName, environment.streets) > -1) {
 				$.post(site + '/locations.json?callback=?', {name: name, number: number, street: streetName, borough: borough, state: state, fullcity: fullcity}, function(data) {
@@ -38,14 +38,14 @@ define(['jquery'], function($) {
 		resp(filtered);
 	}
 
-	function loadContent(site) {
+	function loadContent() {
 		//just for testing
 		if (environment.borough === 'testset') {
 			environment.borough = 'brooklyn'
 		}
 		//testing is now complete
 
-		$.getJSON(site + '/streets/' + environment.borough + '.json?callback=?', function(data) {
+		$.getJSON(urlpath + '/streets/' + environment.borough + '.json?callback=?', function(data) {
 			environment.streets = data.streets;
 			$('#state_hidden').val(data.state);
 			$('#fullcity_hidden').val(data.fullcity);
@@ -56,6 +56,28 @@ define(['jquery'], function($) {
 		$("#frm-modal-street").autocomplete({
 			source: callback
 		});
+
+		$('.ui-autocomplete').on('click', function() { 
+			var streetName = $('#frm-modal-street').val() || null;
+			if ($.inArray(streetName, environment.streets) > -1) {
+				$('#submitED').removeClass('disabled');
+			}
+			else {
+				$('#submitED').addClass('disabled');
+			}			
+		});
+
+		$('#frm-modal-street').on('keyup', function() {
+			var streetName = $('#frm-modal-street').val() || null;
+			if ($.inArray(streetName, environment.streets) > -1) {
+				$('#submitED').removeClass('disabled');
+			}
+			else {
+				$('#submitED').addClass('disabled');
+			}			
+		});
+
+		onSubmitModal(urlpath);
 	}
 
 	/** Return instantiated function */
