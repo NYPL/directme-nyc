ef44965b367163bd29cee46643b50d453ae1b567
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2012-03-19 18:53:37 -0400

    Results with cutout

--- public/js/modules/results.js
+++ public/js/modules/results.js
@@ -1,7 +1,7 @@
 define(['jquery'], function($) {
 
 	var urlpath = window.location.protocol + "//" + window.location.host;
-
+	
 	function _init() {
 		EDcall(getUrlVar('token'));
 	}
@@ -21,7 +21,38 @@ define(['jquery'], function($) {
 					 data.fullcity_id + "-" + data.eds[i] + "</a>";
 			}
 			$('#EDlist').append(results);
+			showMaps();
 		});
+	}
+	
+	function showMaps() {
+		var lat = 40.721;
+		var lon = -73.979;
+		wax.tilejson('http://a.tiles.mapbox.com/v3/nypllabs.1940topo.jsonp',
+			function(tilejson) {
+				var map = new L.Map('nyplmap', {zoomControl: false, trackResize: false}).addLayer(
+					new wax.leaf.connector(tilejson))
+					.setView(new L.LatLng(lat, lon), 14);
+				map.attributionControl.addAttribution("Map &copy; USGS");
+				var CensusIcon = L.Icon.extend({
+					iconUrl: 'images/marker.png',
+					shadowUrl: 'images/marker-shadow.png',
+					iconSize: new L.Point(43, 35),
+					shadowSize: new L.Point(43, 35),
+					iconAnchor: new L.Point(11, 35),
+					popupAnchor: new L.Point(-3, -76)
+				});
+				var markerIcon = new CensusIcon();
+				var centerMarker = new L.Marker(new L.LatLng(lat, lon), {icon: markerIcon});
+				map.addLayer(centerMarker);
+				
+				// disable interaction
+				map.dragging.disable();
+				map.touchZoom.disable();
+				map.scrollWheelZoom.disable();
+				map.doubleClickZoom.disable();
+			}
+		);
 	}
 
 	function getUrlVar(key){
