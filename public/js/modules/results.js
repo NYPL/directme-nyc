d7d6be8190910d909b85c146f02e03c0d99aba4b
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2012-03-19 23:35:43 -0400

    fixes

--- public/js/modules/results.js
+++ public/js/modules/results.js
@@ -3,13 +3,14 @@ define(['jquery'], function($) {
 	var urlpath = window.location.protocol + "//" + window.location.host;
 
 	function _init() {
-		EDcall(getUrlVar('token'));
-
 		Array.prototype.remove = function(from, to) {
 			var rest = this.slice((to || from) + 1 || this.length);
 			this.length = from < 0 ? this.length + from : from;
 			return this.push.apply(this, rest);
 		};
+
+		EDcall(getUrlVar('token'));
+		showMaps();
 
 	}
 
@@ -134,6 +135,36 @@ define(['jquery'], function($) {
 			log(state_results);
 		});
 
+	}
+
+	function showMaps() {
+		var lat = 40.721;
+		var lon = -73.979;
+		wax.tilejson('http://a.tiles.mapbox.com/v3/nypllabs.1940topo.jsonp',
+			function(tilejson) {
+				var map = new L.Map('nyplmap', {zoomControl: false, trackResize: false}).addLayer(
+					new wax.leaf.connector(tilejson))
+					.setView(new L.LatLng(lat, lon), 14);
+				map.attributionControl.addAttribution("Map &copy; USGS");
+				var CensusIcon = L.Icon.extend({
+					iconUrl: 'images/marker.png',
+					shadowUrl: 'images/marker-shadow.png',
+					iconSize: new L.Point(43, 35),
+					shadowSize: new L.Point(43, 35),
+					iconAnchor: new L.Point(11, 35),
+					popupAnchor: new L.Point(-3, -76)
+				});
+				var markerIcon = new CensusIcon();
+				var centerMarker = new L.Marker(new L.LatLng(lat, lon), {icon: markerIcon});
+				map.addLayer(centerMarker);
+
+				// disable interaction
+				map.dragging.disable();
+				map.touchZoom.disable();
+				map.scrollWheelZoom.disable();
+				map.doubleClickZoom.disable();
+			}
+		);
 	}
 
 	function getUrlVar(key){
