bdf1526fdeecac696d9515c76ba6785eaceba741
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2012-03-23 16:58:13 -0400

    ie:double map test

--- public/js/modules/latest.js
+++ public/js/modules/latest.js
@@ -51,9 +51,6 @@ define(['jquery'], function($) {
 				var map = new L.Map('bigmap', {zoomControl: false, trackResize: false, maxBounds:bounds}).addLayer(
 						new wax.leaf.connector(tilejson));
 					
-				var map2 = new L.Map('bigmap2', {zoomControl: false, trackResize: false, maxBounds:bounds}).addLayer(
-						new wax.leaf.connector(tilejson));
-					
 				var CensusIcon = L.Icon.extend({
 					iconUrl: 'images/marker.png',
 					shadowUrl: 'images/marker-shadow.png',
@@ -86,6 +83,48 @@ define(['jquery'], function($) {
 				map.doubleClickZoom.disable();
 			}
 		);
+		wax.tilejson('http://a.tiles.mapbox.com/v3/nypllabs.nyc1940-11.jsonp',
+				function(tilejson) {
+					// limit bounds of map
+					var NE = new L.LatLng(41.0053,-74.4234),
+				    SW = new L.LatLng(40.3984,-73.5212),
+				    bounds = new L.LatLngBounds(SW, NE);
+
+					var map = new L.Map('bigmap2', {zoomControl: false, trackResize: false, maxBounds:bounds}).addLayer(
+							new wax.leaf.connector(tilejson));
+						
+					var CensusIcon = L.Icon.extend({
+						iconUrl: 'images/marker.png',
+						shadowUrl: 'images/marker-shadow.png',
+						iconSize: new L.Point(43, 35),
+						shadowSize: new L.Point(43, 35),
+						iconAnchor: new L.Point(11, 35),
+						popupAnchor: new L.Point(2, -35)
+					});
+					
+					var markerIcon = new CensusIcon();
+					
+					// add markers
+					for (var i=0;i<loc.length;++i) {
+						var it = loc[i];
+						var m = new L.Marker(new L.LatLng(it.coordinates.lat, it.coordinates.lng), {icon: markerIcon});
+						map.addLayer(m);
+						m.bindPopup("<strong><a href='" + it.url + "'>" + it.address + "</a></strong><br />Found " + it.time_ago + " ago");
+					}
+					
+					// center on last marker
+					map.setView(new L.LatLng(40.6537555, -73.9867), 11);
+					//map.setView(new L.LatLng(loc[loc.length-1].coordinates.lat, loc[loc.length-1].coordinates.lng), 11);
+					
+					map.attributionControl.addAttribution("<a href='http://www.nypl.org/locations/schwarzman/map-division/fire-insurance-topographic-zoning-property-maps-nyc' title='open in new window' target='_blank'>Find more maps in the Lionel Pincus & Princess Firyal Map Division</a>");
+					
+					// disable zooming interaction
+					//map.dragging.disable();
+					map.touchZoom.disable();
+					map.scrollWheelZoom.disable();
+					map.doubleClickZoom.disable();
+				}
+			);
 	}
 	
 	function addStories(stories) {
