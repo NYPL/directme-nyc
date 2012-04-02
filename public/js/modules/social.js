73d9e328a369c7a0f87fb7fcc4f198d083a35a7a
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2012-04-01 21:53:20 -0400

    auth fail back

--- public/js/modules/social.js
+++ public/js/modules/social.js
@@ -60,8 +60,9 @@ define(['jquery'], function($) {
 
 			var timer = setInterval(function() {   
 			    if(auth_window.closed) {
-			    	clearInterval(timer);   
-					checkSession(); 
+			    	clearInterval(timer);
+			    	var stopback = true;   
+					checkSession(stopback); 
 			    }  
 			}, 200);
 		}
@@ -88,7 +89,7 @@ define(['jquery'], function($) {
 	}
 
 
-	function checkSession() {
+	function checkSession(stopback) {
 		$.getJSON(urlpath + '/api/session.json?callback=?', function(data) {
 			if (typeof data !== 'undefined' && data.hasOwnProperty('sess') && data.sess !== false) {
 				environment.login = true;
@@ -109,6 +110,13 @@ define(['jquery'], function($) {
 					updateStory(data.conn, author, data.sess);
 				});
 			}
+
+			else if (typeof stopback !== 'undefined') {
+				environment.login = false;
+				var false_auth = true
+				appendError(false_auth);
+			}
+
 			else {
 				environment.login = false;
 			}
